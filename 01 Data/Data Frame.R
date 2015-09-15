892cbc2b05c70e578a92d45ec3cee2d4d119c1b8
Ryan Wechter <ryan.wechter@utexas.edu>
2015-09-15 14:19:35 -0500

    Submission

--- 01 Data/Data Frame.R
+++ 01 Data/Data Frame.R
@@ -44,7 +44,7 @@ ggplot() +
   #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="SURVIVED", y=paste("FARE")) +
-  layer(data=df, 
+  layer(data=subset(df,SEX != "null"), 
         mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED)), 
         stat="identity", 
         stat_params=list(), 
@@ -89,3 +89,20 @@ ggplot() +
         #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
+
+###Unique Plot
+ggplot() + 
+  coord_cartesian() + 
+  scale_x_continuous() +
+  scale_y_continuous() +
+  labs(title='Titanic') +
+  labs(x="PASSENGER ID", y="TICKET NUMBER") +
+  layer(data=subset(df,SEX != "null"), 
+        mapping=aes(x=as.numeric(PASSENGERID), y=as.numeric(TICKET), color=SEX), 
+        stat="identity", 
+        stat_params=list(), 
+        geom="point",
+        geom_params=list(), 
+        position=position_identity()
+        #position=position_jitter(width=0.3, height=0)
+  ) 
